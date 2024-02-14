24a7ad3a77eb604c3d6c96ff2d7ff12608ff7c59
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-02-14 14:31:52 -0300

    update classe Diretor - retirada de atributo tempoDeCargo

--- src/Diretor.java
+++ src/Diretor.java
@@ -1,16 +1,8 @@
 public class Diretor extends Funcionario {
 
-    private int tempoDeCargo;
 
     public Diretor(String nome, double salario){
         super(nome, salario);
     }
 
-    public int getTempoDeCargo() {
-        return tempoDeCargo;
-    }
-
-    public void setTempoDeCargo(int tempoDeCargo) {
-        this.tempoDeCargo = tempoDeCargo;
-    }
 }
