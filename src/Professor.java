94d499f17cec7a4b5fd908d3b34713b8fcacb0e0
Lucas Mateus Peloso Silva <115708507+lucaspeloso22@users.noreply.github.com>
2024-02-13 22:03:37 -0300

    Encapsulamento e implementação dos getters e setters

--- src/Professor.java
+++ src/Professor.java
@@ -3,6 +3,36 @@ class Professor {
     private int idade;
     private int tempoDeTrabalho;
 
+    public Professor(String nome, int idade, int tempoDeTrabalho) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tempoDeTrabalho = tempoDeTrabalho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public int getTempoDeTrabalho() {
+        return tempoDeTrabalho;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setTempoDeTrabalho(int tempoDeTrabalho) {
+        this.tempoDeTrabalho = tempoDeTrabalho;
+    }
 }
+
 
 
