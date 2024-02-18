e0d36a40f2926667496d25202221b61d8b3fd8d8
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-02-17 21:18:25 -0300

    criação classe DadosDiretor e update da classe Diretor

---
+++ src/Funcionario/DadosDiretor.java
@@ -0,0 +1,37 @@
+package Funcionario;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class DadosDiretor {
+
+    private static List<Diretor> listaDiretor = new ArrayList<>();
+
+    public static void adicionarDiretor(Diretor diretor) {
+        listaDiretor.add(diretor);
+    }
+    // o metodo adicionarDiretor() é chamado dentro do bloco de construtor da classe Diretor
+    // sempre que um Diretor for instanciado, é adicionado a listaDiretor
+
+    public static void removerDiretor(int id) {
+        try {
+            listaDiretor.remove(id);
+            System.out.println("Diretor removido da lista!");
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Esse ID não pertence a nenhum diretor listado");
+        }
+
+    }
+
+    public static void buscaDiretor(int id) {
+        try {
+            Diretor diretorEncontrado = listaDiretor.get(id);
+            System.out.println(diretorEncontrado);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Esse ID não pertence a nenhum diretor listado");
+        }
+    }
+
+
+}
--- src/Funcionario/Diretor.java
+++ src/Funcionario/Diretor.java
@@ -4,10 +4,12 @@ public class Diretor extends Funcionario {
 
     public Diretor(String nome, double salario, int tempoDeCargo, EnumCargoFuncionario cargo){
         super(nome, salario, tempoDeCargo, cargo);
+        DadosDiretor.adicionarDiretor(this);
     }
 
     public Diretor(String nome, int tempoDeCargo){
         super(nome, tempoDeCargo);
+        DadosDiretor.adicionarDiretor(this);
     }
     @Override
     public String toString(){
