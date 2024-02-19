632a6a7f1234b80e91bcd89f8fb305a5a540e5a9
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-02-19 16:37:42 -0300

    update metodo de retorno em DadosProfessores

--- src/Funcionario/DadosProfessor.java
+++ src/Funcionario/DadosProfessor.java
@@ -6,26 +6,41 @@ import java.util.List;
 public class DadosProfessor {
     private static List<Professor> listaDeProfessores = new ArrayList<>();
 
-    public void adicionarProfessores(Professor professor) {
+    public static void adicionarProfessores(Professor professor) {
         listaDeProfessores.add(professor);
     }
 
-    public static void removerProfessores (int id) {
+    public static Professor removerProfessores (int id) {
         try {
-            listaDeProfessores.remove(id);
+            Professor professorRemovido = listaDeProfessores.remove(id);
             System.out.println("Professor removido com sucesso.");
+            return professorRemovido;
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Esse ID não pertence a nenhum professor listado.");
+            return null;
         }
 
     }
 
-    public static void bucarProfessor (int id) {
+    public static Professor bucarProfessor (int id) {
         try {
             Professor professorEncontrado = listaDeProfessores.get(id);
             System.out.println("ID: " + id + "\n" + professorEncontrado);
+            return professorEncontrado;
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Esse ID não pertence a nenhum professor listado.");
+            return null;
+        }
+    }
+
+    public static void mostrarProfessoresListados() {
+        if(listaDeProfessores.isEmpty()){
+            System.out.println("Não há nenhum professor listado.");
+        } else {
+            System.out.println("Professores listados:");
+            for(int i = 0; i < listaDeProfessores.size(); i++){
+                System.out.println("\nID: " + i + "\n" + listaDeProfessores.get(i));
+            }
         }
     }
 
--- src/Funcionario/Professor.java
+++ src/Funcionario/Professor.java
@@ -6,19 +6,21 @@ public class Professor extends Funcionario {
     public Professor(String nome, double salario, int tempoDeCargo, EnumCargoFuncionario cargo , int idade) {
         super(nome, salario, tempoDeCargo, cargo);
         this.idade = idade;
+        DadosProfessor.adicionarProfessores(this);
     }
 
     public Professor(String nome, int tempoDeCargo, int idade){
         super(nome, tempoDeCargo);
         this.idade = idade;
+        DadosProfessor.adicionarProfessores(this);
     }
 
     @Override
     public String toString(){
-        return "Nome: " + getNome() + "/" + "Idade: " + idade +
-                "/" + "Salário" + getSalario() +
-                "/" + "Tempo de cargo: " + getTempoDeCargo() +
-                "/" + "Cargo: " + getCargo();
+        return "Nome: " + getNome() + "\nIdade: " + idade +
+                "\nSalário" + getSalario() +
+                "\nTempo de cargo: " + getTempoDeCargo() +
+                "\nCargo: " + getCargo();
     }
 
     public int getIdade() {
